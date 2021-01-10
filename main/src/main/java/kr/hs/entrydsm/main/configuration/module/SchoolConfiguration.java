b3b3073353ab759eb6b0e9497ad353f9dd5e1bfd
LeagueLugas <leaguelugas@gmail.com>
2021-01-10 16:55:29 +0900

    [DELETE] school module

--- main/src/main/java/kr/hs/entrydsm/main/configuration/module/SchoolConfiguration.java
+++
@@ -1,9 +0,0 @@
-package kr.hs.entrydsm.main.configuration.module;
-
-import kr.hs.entrydsm.main.configuration.DependentModule;
-import kr.hs.entrydsm.school.EnableSchoolModule;
-
-@EnableSchoolModule
-@DependentModule(ApplicationConfiguration.class)
-public class SchoolConfiguration {
-}
