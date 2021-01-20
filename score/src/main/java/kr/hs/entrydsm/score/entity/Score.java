f16fe3c68f25586e0cb6b7973a4b10232672b153
JeongWooYeong <wjd030811@gmail.com>
2021-01-20 15:39:51 +0900

    [ADD] score entity

--- score/src/main/java/kr/hs/entrydsm/score/entity/Score.java
+++ score/src/main/java/kr/hs/entrydsm/score/entity/Score.java
@@ -1,19 +1,15 @@
 package kr.hs.entrydsm.score.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import kr.hs.entrydsm.score.integrate.application.ApplicationCase;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 @Getter
-@Builder
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity(name = "tbl_calculated_score")
 public class Score {
 
@@ -25,18 +21,17 @@ public class Score {
 
     private Integer attendanceScore;
 
-    @Digits(integer = 2, fraction = 3)
-    private BigDecimal firstGradeScore;
+    @Digits(integer = 3, fraction = 3)
+    private BigDecimal gradeScore;
 
-    @Digits(integer = 2, fraction = 3)
-    private BigDecimal secondGradeScore;
+    @Digits(integer = 3, fraction = 3)
+    private BigDecimal totalScore;
 
-    @Digits(integer = 2, fraction = 3)
-    private BigDecimal thirdGradeScore;
-
-    @Column
-    private LocalDateTime createdAt;
-
-    @Column
-    private LocalDateTime modifiedAt;
+    public Score(long receiptCode, ApplicationCase applicationCase) {
+        this.receiptCode = receiptCode;
+        this.volunteerScore = applicationCase.calculateVolunteerScore();
+        this.attendanceScore = applicationCase.calculateAttendanceScore();
+        this.gradeScore = applicationCase.calculateGradeScore();
+        this.totalScore = volunteerScore.add(BigDecimal.valueOf(attendanceScore)).add(gradeScore);
+    }
 }
--- score/src/main/java/kr/hs/entrydsm/score/entity/ScoreRepository.java
+++ score/src/main/java/kr/hs/entrydsm/score/entity/ScoreRepository.java
@@ -1,5 +1,5 @@
 package kr.hs.entrydsm.score.entity;
 
 public interface ScoreRepository {
-
+    Score save(Score score);
 }
