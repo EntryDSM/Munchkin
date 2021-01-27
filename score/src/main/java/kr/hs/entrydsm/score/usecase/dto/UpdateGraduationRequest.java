5da99357428f5b96155c9956cffae5abaf820a42
JeongWooYeong <wjd030811@gmail.com>
2021-01-27 13:52:25 +0900

    [ADD] dto

---
+++ score/src/main/java/kr/hs/entrydsm/score/usecase/dto/UpdateGraduationRequest.java
@@ -0,0 +1,47 @@
+package kr.hs.entrydsm.score.usecase.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.PositiveOrZero;
+
+@Getter
+@AllArgsConstructor
+public class UpdateGraduationRequest {
+    @PositiveOrZero
+    private final int volunteerTime;
+
+    @PositiveOrZero
+    private final int dayAbsenceCount;
+
+    @PositiveOrZero
+    private final int lectureAbsenceCount;
+
+    @PositiveOrZero
+    private final int latenessCount;
+
+    @PositiveOrZero
+    private final int earlyLeaveCount;
+
+    @Pattern(regexp = "[A-E,X]{6}")
+    private final String koreanGrade;
+
+    @Pattern(regexp = "[A-E,X]{6}")
+    private final String socialGrade;
+
+    @Pattern(regexp = "[A-E,X]{6}")
+    private final String historyGrade;
+
+    @Pattern(regexp = "[A-E,X]{6}")
+    private final String mathGrade;
+
+    @Pattern(regexp = "[A-E,X]{6}")
+    private final String scienceGrade;
+
+    @Pattern(regexp = "[A-E,X]{6}")
+    private final String englishGrade;
+
+    @Pattern(regexp = "[A-E,X]{6}")
+    private final String techAndHomeGrade;
+}
