1d9064f10368212f4488b9496374ca5ffcda619f
LeagueLugas <leaguelugas@gmail.com>
2021-02-01 12:22:28 +0900

    [FIX] User 엔티티

--- user/src/main/java/kr/hs/entrydsm/user/domain/entity/User.java
+++ user/src/main/java/kr/hs/entrydsm/user/domain/entity/User.java
@@ -1,14 +1,18 @@
 package kr.hs.entrydsm.user.domain.entity;
 
+import kr.hs.entrydsm.user.domain.entity.enumeration.ApplicationRemark;
+import kr.hs.entrydsm.user.domain.entity.enumeration.ApplicationType;
+import kr.hs.entrydsm.user.domain.entity.enumeration.EducationalStatus;
+import kr.hs.entrydsm.user.domain.entity.enumeration.Sex;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @Builder
@@ -21,10 +25,57 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long receiptCode;
 
-    private String telephone_number;
+    @Column(columnDefinition = "char (11)", nullable = false)
+    private String telephoneNumber;
 
+    @Column(columnDefinition = "char(11)", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private ApplicationType applicationType;
+
+    @Enumerated(EnumType.STRING)
+    private ApplicationRemark applicationRemark;
+
+    @Enumerated(EnumType.STRING)
+    private EducationalStatus educationalStatus;
+
+    private boolean isDaejeon;
+
+    @Column(columnDefinition = "char(5)", nullable = false)
     private String name;
 
-    private String password;
+    @Enumerated(EnumType.STRING)
+    private Sex sex;
+
+    private LocalDate birthday;
+
+    @Column(columnDefinition = "char(5)")
+    private String parentName;
+
+    @Column(columnDefinition = "char(11)")
+    private String parentTel;
+
+    @Column(length = 300)
+    private String address;
+
+    @Column(columnDefinition = "char(5)")
+    private String postCode;
+
+    @Column(columnDefinition = "char(45)")
+    private String photoFileName;
+
+    @Column(columnDefinition = "char(11)")
+    private String homeTel;
+
+    @Column(length = 1600)
+    private String selfIntroduce;
+
+    @Column(length = 1600)
+    private String studyPlan;
+
+    @CreatedBy
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
 
 }
