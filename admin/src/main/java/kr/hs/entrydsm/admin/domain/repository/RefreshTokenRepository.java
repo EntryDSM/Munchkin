393c4c5b134b9b364bdddffd95e39bb2b807af66
이승윤 <yyuunn17@naver.com>
2021-02-06 17:35:46 +0900

    [ADD] 로그인, 토큰 재발급

--- admin/src/main/java/kr/hs/entrydsm/admin/domain/repository/RefreshTokenRepository.java
+++ admin/src/main/java/kr/hs/entrydsm/admin/domain/repository/RefreshTokenRepository.java
@@ -1,9 +1,10 @@
 package kr.hs.entrydsm.admin.domain.repository;
 
 import kr.hs.entrydsm.admin.domain.entity.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface RefreshTokenRepostory {
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Integer> {
     Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
---
+++ admin/src/main/java/kr/hs/entrydsm/admin/usecase/AuthServiceManager.java
@@ -0,0 +1,62 @@
+package kr.hs.entrydsm.admin.usecase;
+
+import kr.hs.entrydsm.admin.domain.entity.Admin;
+import kr.hs.entrydsm.admin.domain.entity.RefreshToken;
+import kr.hs.entrydsm.admin.domain.repository.AdminRepository;
+import kr.hs.entrydsm.admin.domain.repository.RefreshTokenRepository;
+import kr.hs.entrydsm.admin.security.JwtTokenProvider;
+import kr.hs.entrydsm.admin.usecase.dto.AccessTokenResponse;
+import kr.hs.entrydsm.admin.usecase.dto.SignInRequest;
+import kr.hs.entrydsm.admin.usecase.dto.TokenResponse;
+import kr.hs.entrydsm.admin.usecase.exception.AdminNotFoundException;
+import kr.hs.entrydsm.admin.usecase.exception.InvalidTokenException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthServiceManager implements AuthService {
+
+    private final AdminRepository adminRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Value("${auth.jwt.exp.refresh}")
+    private Long refreshExp;
+
+    @Value("${auth.jwt.exp.access}")
+    private Long accessExp;
+
+    @Override
+    public TokenResponse login(SignInRequest signInRequest) {
+        return adminRepository.findById(signInRequest.getId())
+                .filter(user -> passwordEncoder.matches(signInRequest.getPassword(), user.getPassword()))
+                .map(Admin::getId)
+                .map(adminId -> {
+                    String refreshToken = jwtTokenProvider.generateRefreshToken(adminId.toString());
+                    return new RefreshToken(adminId.toString(), refreshToken, refreshExp);
+                })
+                .map(refreshTokenRepository::save)
+                .map(refreshToken -> {
+                    String accessToken = jwtTokenProvider.generateAccessToken(refreshToken.getId());
+                    return new TokenResponse(accessToken, refreshToken.getRefreshToken(), accessExp);
+                })
+                .orElseThrow(AdminNotFoundException::new);
+    }
+
+    @Override
+    public AccessTokenResponse tokenRefresh(String receivedToken) {
+        if(!jwtTokenProvider.isRefreshToken(receivedToken)) {
+            throw new InvalidTokenException();
+        }
+
+        return refreshTokenRepository.findByRefreshToken(receivedToken)
+                .map(refreshToken ->
+                        new AccessTokenResponse(jwtTokenProvider.generateAccessToken(refreshToken.getId())))
+                .orElseThrow(AdminNotFoundException::new);
+    }
+}
