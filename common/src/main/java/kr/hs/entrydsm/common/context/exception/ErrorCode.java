5d28665751d21df7bb300c6e8eea3c4b468a4691
이승윤 <yyuunn17@naver.com>
2021-02-06 16:02:16 +0900

    [UPDATE] 어드민에서 쓸 exception 추가

--- common/src/main/java/kr/hs/entrydsm/common/context/exception/ErrorCode.java
+++ common/src/main/java/kr/hs/entrydsm/common/context/exception/ErrorCode.java
@@ -8,7 +8,11 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    APPLICATION_TYPE_UNMATCHED(403, "SCORE403-0", "Application Type is unmatched");
+    APPLICATION_TYPE_UNMATCHED(403, "SCORE403-0", "Application Type is unmatched"),
+
+    INVALID_TOKEN(401, "ADMIN401-0","This token is invalid"),
+    USER_NOT_FOUND(404, "ADMIN404-0","The user does not exist"),
+    APPLICATION_NOT_FOUND(404, "ADMIN404-1","The application could not be found");
 
     private final int status;
     private final String code;
