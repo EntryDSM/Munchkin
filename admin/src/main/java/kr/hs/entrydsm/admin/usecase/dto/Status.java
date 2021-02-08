f067083b4652aecfb56762f4dcafffe673f42bdb
이승윤 <yyuunn17@naver.com>
2021-02-08 12:39:35 +0900

    [UPDATE] 어노테이션 추가

--- admin/src/main/java/kr/hs/entrydsm/admin/usecase/dto/Status.java
+++ admin/src/main/java/kr/hs/entrydsm/admin/usecase/dto/Status.java
@@ -1,10 +1,12 @@
 package kr.hs.entrydsm.admin.usecase.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Status {
