0fe1619247fde9d7708254c2c803316cb1a313fc
이승윤 <yyuunn17@naver.com>
2021-02-08 14:26:42 +0900

    [ADD] admin401 error 추가

--- common/src/main/java/kr/hs/entrydsm/common/context/exception/ErrorCode.java
+++ common/src/main/java/kr/hs/entrydsm/common/context/exception/ErrorCode.java
@@ -14,7 +14,8 @@ public enum ErrorCode {
     EXPIRED_TOKEN(401, "ADMIN401-1","Token is Expired"),
     ADMIN_NOT_FOUND(404, "ADMIN404-0","The account does not exist"),
     APPLICATION_NOT_FOUND(404, "ADMIN404-1","The application could not be found"),
-    APPLICANT_NOT_FOUND(404, "ADMIN404-2","The applicant does not exist");
+    APPLICANT_NOT_FOUND(404, "ADMIN404-2","The applicant does not exist"),
+    NOT_ACCESSIBLE(401, "ADMIN401-2","Check the token");
 
     private final int status;
     private final String code;
