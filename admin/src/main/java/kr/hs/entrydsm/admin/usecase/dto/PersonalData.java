600596ba933d682b501a959807734fb9f82c3670
이승윤 <yyuunn17@naver.com>
2021-02-08 13:01:53 +0900

    [UPDATE] 사용하지 않는 import 삭제

--- admin/src/main/java/kr/hs/entrydsm/admin/usecase/dto/PersonalData.java
+++ admin/src/main/java/kr/hs/entrydsm/admin/usecase/dto/PersonalData.java
@@ -1,9 +1,7 @@
 package kr.hs.entrydsm.admin.usecase.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
