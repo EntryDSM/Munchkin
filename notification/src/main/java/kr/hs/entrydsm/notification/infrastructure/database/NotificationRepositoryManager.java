1859a0023e7aa9513ae59330873d1834be861eb1
이승윤 <yyuunn17@naver.com>
2021-02-11 14:45:29 +0900

    [UPDATE] 공지메세지 repository jpa->crud

--- notification/src/main/java/kr/hs/entrydsm/notification/infrastructure/database/NotificationRepositoryManager.java
+++ notification/src/main/java/kr/hs/entrydsm/notification/infrastructure/database/NotificationRepositoryManager.java
@@ -3,14 +3,14 @@ package kr.hs.entrydsm.notification.infrastructure.database;
 import kr.hs.entrydsm.notification.domain.entity.Notification;
 import kr.hs.entrydsm.notification.domain.entity.Type;
 import kr.hs.entrydsm.notification.domain.repository.NotificationRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface NotificationRepositoryManager extends JpaRepository<Notification, Type>, NotificationRepository {
+public interface NotificationRepositoryManager extends CrudRepository<Notification, Type>, NotificationRepository {
 
     @Override
     Optional<Notification> findByType(Type type);
