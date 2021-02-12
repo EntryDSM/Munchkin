0b3c6728763de625685bbdcbd56c163b52f3a516
leeseojune <sung07288346@gmail.com>
2021-02-12 23:57:37 +0900

    [ADD] ApplicationRepository

---
+++ application/src/main/java/kr/hs/entrydsm/application/integrate/user/ApplicantRepository.java
@@ -0,0 +1,13 @@
+package kr.hs.entrydsm.application.integrate.user;
+
+import kr.hs.entrydsm.application.usecase.dto.Application;
+import kr.hs.entrydsm.application.usecase.dto.Information;
+import kr.hs.entrydsm.common.context.beans.Published;
+
+@Published
+public interface ApplicantRepository {
+    void writeApplicationType(Long receiptCode, Application application);
+    void writeInformation(Long receiptCode, Information information);
+    Application getApplicationType(Long receiptCode);
+    Information getInformation(Long receiptCode);
+}
