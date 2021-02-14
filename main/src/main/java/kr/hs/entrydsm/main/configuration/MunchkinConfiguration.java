bf0158473777e3b25d763b875bc6e38183e373f3
LeagueLugas <leaguelugas@gmail.com>
2021-02-14 16:11:22 +0900

    [ADD] scanable package

--- main/src/main/java/kr/hs/entrydsm/main/configuration/MunchkinConfiguration.java
+++ main/src/main/java/kr/hs/entrydsm/main/configuration/MunchkinConfiguration.java
@@ -7,8 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EntityScan("kr.hs.entrydsm.*.domain.entity")
-@EnableJpaRepositories("kr.hs.entrydsm.*.domain.entity")
+@EnableJpaRepositories("kr.hs.entrydsm.*.infrastructure.database")
 @ComponentScan("kr.hs.entrydsm.common.context")
+@ComponentScan("kr.hs.entrydsm.main.security")
 @SpringBootConfiguration
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "kr.hs.entrydsm.main.integrate", lazyInit = true)
