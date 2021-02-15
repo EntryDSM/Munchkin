9e7a5595167869e7e724c03612ada2414e21d15a
LeagueLugas <leaguelugas@gmail.com>
2021-02-15 20:55:33 +0900

    [UPDATE] web config

--- main/src/main/java/kr/hs/entrydsm/main/security/WebConfig.java
+++ main/src/main/java/kr/hs/entrydsm/main/security/WebConfig.java
@@ -2,11 +2,14 @@ package kr.hs.entrydsm.main.security;
 
 import kr.hs.entrydsm.main.security.handler.JWTTokenHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -33,7 +36,12 @@ public class WebConfig extends WebSecurityConfigurerAdapter implements WebMvcCon
                 .sessionManagement()
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
-                .anyRequest().permitAll();
+                    .anyRequest().permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
