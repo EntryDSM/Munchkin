9247d32f55e988c15787b33a908fe129be237b75
김대웅 <dkssud9556@dsm.hs.kr>
2021-02-20 21:09:46 +0900

    [UPDATE] optimize imports

--- application/src/main/java/kr/hs/entrydsm/application/integrate/admin/ApplicationExportRepository.java
+++ application/src/main/java/kr/hs/entrydsm/application/integrate/admin/ApplicationExportRepository.java
@@ -1,7 +1,6 @@
 package kr.hs.entrydsm.application.integrate.admin;
 
 import kr.hs.entrydsm.common.model.ReportCard;
-import kr.hs.entrydsm.common.context.beans.Published;
 
 public interface ApplicationExportRepository {
 
