386f2347ca719a4f4128362e46655ee3a29a63b5
이승윤 <yyuunn17@naver.com>
2021-02-22 16:45:01 +0900

    [DELETE] repository

--- admin/src/main/java/kr/hs/entrydsm/admin/integrate/user/ScoreRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.hs.entrydsm.admin.integrate.user;
-
-import kr.hs.entrydsm.admin.domain.entity.Applicant;
-
-public interface ScoreRepository {
-    Applicant getUserInfo(long receiptCode);
-}
