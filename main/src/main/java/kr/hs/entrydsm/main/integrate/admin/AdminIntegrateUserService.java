47f0557582304455efb0fe849c7d687012ab5539
이승윤 <yyuunn17@naver.com>
2021-03-02 16:32:57 +0900

    [FIX] integrate 수정

--- main/src/main/java/kr/hs/entrydsm/main/integrate/admin/AdminIntegrateUserService.java
+++ main/src/main/java/kr/hs/entrydsm/main/integrate/admin/AdminIntegrateUserService.java
@@ -2,21 +2,15 @@ package kr.hs.entrydsm.main.integrate.admin;
 
 import kr.hs.entrydsm.admin.domain.entity.Applicant;
 import kr.hs.entrydsm.admin.integrate.user.ApplicantRepository;
-import kr.hs.entrydsm.application.domain.entity.Application;
-import kr.hs.entrydsm.application.domain.entity.QualificationExamApplication;
 import kr.hs.entrydsm.application.integrate.admin.ApplicationExportRepository;
-import kr.hs.entrydsm.common.model.GraduationReportCard;
-import kr.hs.entrydsm.common.model.QualificationReportCard;
 import kr.hs.entrydsm.common.model.ReportCard;
 import kr.hs.entrydsm.user.domain.entity.User;
 import kr.hs.entrydsm.user.integrate.admin.UserExportRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -75,8 +69,7 @@ public class AdminIntegrateUserService implements ApplicantRepository {
     public Applicant getUserInfo(long receiptCode) {
         User user = userExportRepository.findByReceiptCode((int) receiptCode);
 
-        GraduationReportCard graduationReportCard = (GraduationReportCard) applicationExportRepository.getReportCard(receiptCode);
-        QualificationReportCard qualificationReportCard = (QualificationReportCard) applicationExportRepository.getReportCard(receiptCode);
+        ReportCard reportCard = applicationExportRepository.getReportCard(receiptCode);
 
         return Applicant.builder()
                 .receiptCode(user.getReceiptCode())
@@ -95,16 +88,16 @@ public class AdminIntegrateUserService implements ApplicantRepository {
                 .selfIntroduce(user.getSelfIntroduce())
                 .studyPlan(user.getStudyPlan())
                 //수정 필요
-                .averageScore(qualificationReportCard.getAverageScore()) // 검정고시 평균 점수
-                .isGraduated(graduationReportCard.isGraduated()) // 검x 졸업 여부
-                .volunteerTime(graduationReportCard.getVolunteerTime()) // 검X 봉사점수
-                .schoolTel(graduationReportCard.getSchoolTel()) //검x 학교 전화번호
-                .schoolName(graduationReportCard.getSchoolName()) // 검x 학교 이름
-                .latenessCount(graduationReportCard.getLatenessCount()) // 지각 횟수
-                .earlyLeaveCount(graduationReportCard.getEarlyLeaveCount()) // 조퇴 횟수
-                .lectureAbsenceCount(graduationReportCard.getLectureAbsenceCount()) // 무단 결과
-                .dayAbsenceCount(graduationReportCard.getDayAbsenceCount()) // 무단 결석
-                .conversionScore(graduationReportCard.getTotalScore()) // 총 점수
+                .averageScore(reportCard.getAverageScore()) // 검정고시 평균 점수
+                .isGraduated(reportCard.getIsGraduated()) // 검x 졸업 여부
+                .volunteerTime(reportCard.getVolunteerTime()) // 검X 봉사점수
+                .schoolTel(reportCard.getSchoolTel()) //검x 학교 전화번호
+                .schoolName(reportCard.getSchoolName()) // 검x 학교 이름
+                .latenessCount(reportCard.getLatenessCount()) // 지각 횟수
+                .earlyLeaveCount(reportCard.getEarlyLeaveCount()) // 조퇴 횟수
+                .lectureAbsenceCount(reportCard.getLectureAbsenceCount()) // 무단 결과
+                .dayAbsenceCount(reportCard.getDayAbsenceCount()) // 무단 결석
+                .conversionScore(reportCard.getTotalScore()) // 총 점수
                 .build();
     }
 
