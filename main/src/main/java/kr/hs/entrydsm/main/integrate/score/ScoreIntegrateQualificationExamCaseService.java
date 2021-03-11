0a7f5fcb8df61ebcfaa4a615d6b5b8d482ce87fb
LeagueLugas <leaguelugas@gmail.com>
2021-03-11 23:23:51 +0900

    [UPDATE] add @Service annotation

--- main/src/main/java/kr/hs/entrydsm/main/integrate/score/ScoreIntegrateQualificationExamCaseService.java
+++ main/src/main/java/kr/hs/entrydsm/main/integrate/score/ScoreIntegrateQualificationExamCaseService.java
@@ -2,9 +2,11 @@ package kr.hs.entrydsm.main.integrate.score;
 
 import kr.hs.entrydsm.score.integrate.application.QualificationExamCase;
 import kr.hs.entrydsm.score.integrate.application.QualificationExamCaseRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class ScoreIntegrateQualificationExamCaseService implements QualificationExamCaseRepository {
     @Override
     public Optional<QualificationExamCase> findByReceiptCode(long receiptCode) {
