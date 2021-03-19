f91cee8dde341603df5bd98a48ed06955bcae1ca
leeseojune <sung07288346@gmail.com>
2021-03-19 14:53:00 +0900

    [FIX] 구조에 맞지 않던거수정

--- main/src/main/java/kr/hs/entrydsm/main/integrate/application/ApplicationIntegrateApplicantService.java
+++ main/src/main/java/kr/hs/entrydsm/main/integrate/application/ApplicationIntegrateApplicantService.java
@@ -1,16 +1,25 @@
 package kr.hs.entrydsm.main.integrate.application;
 
-import kr.hs.entrydsm.application.integrate.user.ApplicantExportService;
+import kr.hs.entrydsm.application.integrate.user.ApplicationApplicantRepository;
 import kr.hs.entrydsm.application.usecase.dto.Application;
 import kr.hs.entrydsm.application.usecase.dto.Information;
 import kr.hs.entrydsm.common.context.beans.Published;
+import kr.hs.entrydsm.user.entity.User;
+import kr.hs.entrydsm.user.integrate.application.ApplicationUserExportRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Published
-@Service
-public class ApplicationIntegrateApplicantService implements ApplicantExportService {
+@RequiredArgsConstructor
+@Component
+public class ApplicationIntegrateApplicantService implements ApplicationApplicantRepository {
+
+    private ApplicationUserExportRepository userExportRepository;
+
     @Override
     public void writeApplicationType(Long receiptCode, Application application) {
+        userExportRepository.changeApplication(receiptCode, application.getEducationalStatus(),
+                application.getApplicationType(), application.isDaejeon(), application.getApplicationRemark());
 
     }
 
@@ -21,7 +30,13 @@ public class ApplicationIntegrateApplicantService implements ApplicantExportServ
 
     @Override
     public Application getApplicationType(Long receiptCode) {
-        return null;
+        User user = userExportRepository.findByReceiptCode(receiptCode);
+        return Application.builder()
+                .educationalStatus(user.getEducationalStatus().toString())
+                .applicationType(user.getApplicationType().toString())
+                .applicationRemark(user.getApplicationRemark().toString())
+                .isDaejeon(user.isDaejeon())
+                .build();
     }
 
     @Override
