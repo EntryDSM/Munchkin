e072611c0ebf45e202548591bce129d59adc9d49
LeagueLugas <leaguelugas@gmail.com>
2021-03-21 23:29:08 +0900

    [ADD] user register service

--- user/src/main/java/kr/hs/entrydsm/user/usecase/UserService.java
+++ user/src/main/java/kr/hs/entrydsm/user/usecase/UserService.java
@@ -1,9 +1,11 @@
 package kr.hs.entrydsm.user.usecase;
 
 import kr.hs.entrydsm.user.usecase.dto.request.AccountRequest;
+import kr.hs.entrydsm.user.usecase.dto.request.SignupRequest;
 import kr.hs.entrydsm.user.usecase.dto.response.AccessTokenResponse;
 import org.springframework.http.ResponseEntity;
 
 public interface UserService {
     ResponseEntity<AccessTokenResponse> auth(AccountRequest accountRequest);
+    ResponseEntity<AccessTokenResponse> registerUser(SignupRequest signupRequest);
 }
--- user/src/main/java/kr/hs/entrydsm/user/usecase/UserServiceManager.java
+++ user/src/main/java/kr/hs/entrydsm/user/usecase/UserServiceManager.java
@@ -1,10 +1,11 @@
 package kr.hs.entrydsm.user.usecase;
 
 import kr.hs.entrydsm.common.context.auth.token.JWTTokenProvider;
-import kr.hs.entrydsm.user.entity.User;
-import kr.hs.entrydsm.user.entity.UserRepository;
+import kr.hs.entrydsm.user.entity.*;
 import kr.hs.entrydsm.user.usecase.dto.request.AccountRequest;
+import kr.hs.entrydsm.user.usecase.dto.request.SignupRequest;
 import kr.hs.entrydsm.user.usecase.dto.response.AccessTokenResponse;
+import kr.hs.entrydsm.user.usecase.exception.InvalidAuthCodeException;
 import kr.hs.entrydsm.user.usecase.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -17,6 +18,8 @@ import org.springframework.stereotype.Service;
 public class UserServiceManager implements UserService {
 
     private final UserRepository userRepository;
+    private final StatusRepository statusRepository;
+    private final AuthCodeRedisRepository authCodeRepository;
 
     private final PasswordEncoder passwordEncoder;
     private final JWTTokenProvider tokenProvider;
@@ -26,17 +29,50 @@ public class UserServiceManager implements UserService {
         return userRepository.findByTelephoneNumber(accountRequest.getPhoneNumber())
                 .filter(user -> passwordEncoder.matches(accountRequest.getPassword(), user.getPassword()))
                 .map(User::getReceiptCode)
-                .map(receiptCode -> {
-                    String accessToken = tokenProvider.generateAccessToken(receiptCode);
-                    String refreshToken = tokenProvider.generateRefreshToken(receiptCode);
-                    HttpHeaders headers = new HttpHeaders();
-                    headers.set("Set-Cookie", refreshToken);
-
-                    return ResponseEntity.ok()
-                            .headers(headers)
-                            .body(new AccessTokenResponse(accessToken));
-                })
+                .map(this::getAccessToken)
                 .orElseThrow(UserNotFoundException::new);
+    }
+
+    @Override
+    public ResponseEntity<AccessTokenResponse> registerUser(SignupRequest signupRequest) {
+        String phoneNumber = signupRequest.getPhoneNumber();
+        String code = signupRequest.getCode();
+        String name = signupRequest.getName();
+        String password = signupRequest.getPassword();
+        String encodedPassword = passwordEncoder.encode(password);
+
+//        authCodeRepository.findByPhoneNumberAndCode(phoneNumber, code)
+//                .orElseThrow(InvalidAuthCodeException::new);
+
+        User user = userRepository.save(
+                User.builder()
+                        .telephoneNumber(phoneNumber)
+                        .name(name)
+                        .password(encodedPassword)
+                        .build()
+        );
+
+        statusRepository.save(
+                Status.builder()
+                        .receiptCode(user.getReceiptCode())
+                        .isPaid(false)
+                        .isPrintedArrived(false)
+                        .isSubmit(false)
+                        .build()
+        );
+
+        return getAccessToken(user.getReceiptCode());
+    }
+
+    private ResponseEntity<AccessTokenResponse> getAccessToken(long receiptCode) {
+        String accessToken = tokenProvider.generateAccessToken(receiptCode);
+        String refreshToken = tokenProvider.generateRefreshToken(receiptCode);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Set-Cookie", refreshToken);
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(new AccessTokenResponse(accessToken));
     }
 
 }
