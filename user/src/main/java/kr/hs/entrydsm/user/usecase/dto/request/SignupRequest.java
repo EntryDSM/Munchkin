c5e509ec6981bc7e2def3a461542d226d5c5a65c
LeagueLugas <leaguelugas@gmail.com>
2021-03-21 23:29:45 +0900

    [FIX] signup request dto

--- user/src/main/java/kr/hs/entrydsm/user/usecase/dto/request/SignupRequest.java
+++ user/src/main/java/kr/hs/entrydsm/user/usecase/dto/request/SignupRequest.java
@@ -1,22 +1,26 @@
 package kr.hs.entrydsm.user.usecase.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Pattern;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignupRequest extends AccountRequest {
 
     @Pattern(regexp = "[곽-힇]{2,10}")
-    private final String name;
+    private String name;
 
     @Pattern(regexp = "\\d{6}")
-    private final String code;
+    private String code;
 
     public SignupRequest(String phoneNumber, String password, String name, String code) {
         super(phoneNumber, password);
         this.name = name;
         this.code = code;
     }
-    
+
 }
