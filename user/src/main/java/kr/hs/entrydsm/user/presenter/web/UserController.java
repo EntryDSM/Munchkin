f5063463c9f610273955f300bac3ba17d90a5b41
LeagueLugas <leaguelugas@gmail.com>
2021-03-23 22:25:33 +0900

    [ADD] phone verification logic

--- user/src/main/java/kr/hs/entrydsm/user/presenter/web/UserController.java
+++ user/src/main/java/kr/hs/entrydsm/user/presenter/web/UserController.java
@@ -2,6 +2,8 @@ package kr.hs.entrydsm.user.presenter.web;
 
 import kr.hs.entrydsm.common.context.beans.Published;
 import kr.hs.entrydsm.user.usecase.UserService;
+import kr.hs.entrydsm.user.usecase.dto.request.AuthCodeRequest;
+import kr.hs.entrydsm.user.usecase.dto.request.PhoneNumberRequest;
 import kr.hs.entrydsm.user.usecase.dto.request.SignupRequest;
 import kr.hs.entrydsm.user.usecase.dto.response.AccessTokenResponse;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RequestMapping("/user")
@@ -20,8 +24,18 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<AccessTokenResponse> registerUser(@RequestBody SignupRequest signupRequest) {
+    public ResponseEntity<AccessTokenResponse> registerUser(@RequestBody @Valid SignupRequest signupRequest) {
         return userService.registerUser(signupRequest);
+    }
+
+    @PostMapping("/phone/verify")
+    public void sendAuthCode(@RequestBody @Valid PhoneNumberRequest phoneNumberRequest) {
+        userService.sendAuthCode(phoneNumberRequest);
+    }
+
+    @PostMapping("/phone/verify")
+    public void verifyAuthCode(@RequestBody AuthCodeRequest authCodeRequest) {
+        userService.verifyAuthCode(authCodeRequest);
     }
 
 }
