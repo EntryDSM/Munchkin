6ba071bc6de0da5496560715ef8d44625dae000e
김대웅 <dkssud9556@dsm.hs.kr>
2021-03-29 10:11:56 +0900

    [UPDATE] pdf data converter

--- application/src/main/java/kr/hs/entrydsm/application/PdfDataConverter.java
+++ application/src/main/java/kr/hs/entrydsm/application/PdfDataConverter.java
@@ -1,6 +1,8 @@
 package kr.hs.entrydsm.application;
 
 import kr.hs.entrydsm.application.entity.*;
+import kr.hs.entrydsm.application.entity.GraduationApplicationRepository;
+import kr.hs.entrydsm.application.entity.QualificationExamApplicationRepository;
 import kr.hs.entrydsm.application.usecase.exception.ApplicationNotFoundException;
 import lombok.RequiredArgsConstructor;
 
@@ -12,6 +14,7 @@ import java.util.Map;
 public class PdfDataConverter {
 
     private final GraduationApplicationRepository graduationRepository;
+    private final QualificationExamApplicationRepository qualificationExamRepository;
 
     private final Map<String, Object> values = new HashMap<>();
 
@@ -58,7 +61,7 @@ public class PdfDataConverter {
             GraduationApplication application = graduationRepository.findByReceiptCode(applicant.getReceiptCode())
                     .orElseThrow(ApplicationNotFoundException::new);
             values.put("schoolCode", setBlankIfNull(application.getSchoolCode()));
-            //values.put("schoolClass", setBlankIfNull(application.getSchoolClass()));
+            values.put("schoolClass", setBlankIfNull(application.getSchoolClass()));
             values.put("schoolTel", toFormattedPhoneNumber(application.getSchoolTel()));
             values.put("schoolName", setBlankIfNull(application.getSchoolName()));
         } else {
@@ -73,12 +76,47 @@ public class PdfDataConverter {
         values.put("homeTel", toFormattedPhoneNumber(homeTel));
     }
 
+    private void setGraduationClassification(Applicant applicant) {
+        values.putAll(emptyGraduationClassification());
+
+        switch (applicant.getEducationalStatus()) {
+            case "QUALIFICATION_EXAM":
+                // TODO 검정고시 합격 날짜 필요
+                break;
+            case "GRADUATE":
+                graduationRepository.findByReceiptCode(applicant.getReceiptCode())
+                        .filter(graduation -> graduation.getGraduateAt() != null)
+                        .ifPresent(graduation -> {
+                            values.put("graduateYear", String.valueOf(graduation.getGraduateAt().getYear()));
+                            values.put("graduateMonth", String.valueOf(graduation.getGraduateAt().getMonthValue()));
+                        });
+                break;
+            case "PROSPECTIVE_GRADUATE":
+                graduationRepository.findByReceiptCode(applicant.getReceiptCode())
+                        .filter(graduation -> graduation.getGraduateAt() != null)
+                        .ifPresent(graduation -> {
+                            values.put("prospectiveGraduateMonth", String.valueOf(graduation.getGraduateAt().getMonthValue()));
+                        });
+                break;
+        }
+    }
+
     private Map<String, Object> emptySchoolInfo() {
         return Map.of(
                 "schoolCode", "",
                 "schoolClass", "",
                 "schoolTel", "",
                 "schoolName", ""
+        );
+    }
+
+    private Map<String, Object> emptyGraduationClassification() {
+        return Map.of(
+                "qualificationExamPassedYear", "20__",
+                "qualificationExamPassedMonth", "__",
+                "graduateYear", "20__",
+                "graduateMonth", "__",
+                "prospectiveGraduateMonth", "__"
         );
     }
 
