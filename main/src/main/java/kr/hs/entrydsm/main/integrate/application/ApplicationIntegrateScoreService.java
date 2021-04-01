e28f7094af487def23938ebd9dfeaff34b59ca40
leeseojune <sung07288346@gmail.com>
2021-04-01 22:26:06 +0900

    [UPDATE] 점수 가져오는 로직

--- main/src/main/java/kr/hs/entrydsm/main/integrate/application/ApplicationIntegrateScoreService.java
+++ main/src/main/java/kr/hs/entrydsm/main/integrate/application/ApplicationIntegrateScoreService.java
@@ -1,13 +1,21 @@
 package kr.hs.entrydsm.main.integrate.application;
 
 import kr.hs.entrydsm.application.integrate.score.ScoreCalculator;
+import kr.hs.entrydsm.application.usecase.dto.Score;
 import kr.hs.entrydsm.common.context.beans.Published;
 import kr.hs.entrydsm.common.model.Scores;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Published
 @Service
 public class ApplicationIntegrateScoreService implements ScoreCalculator {
+
+    @Override
+    public List<Score> getAll() {
+        return null;
+    }
 
     @Override
     public Scores getScores(Long receiptCode) {
