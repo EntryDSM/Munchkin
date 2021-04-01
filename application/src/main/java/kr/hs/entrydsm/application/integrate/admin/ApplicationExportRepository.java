820b9fa045b6fc38f81e51bcc8a57b148012f8bf
leeseojune <sung07288346@gmail.com>
2021-04-01 20:25:05 +0900

    [UPDATE] ApplicationExportRepository 함수 추가

--- application/src/main/java/kr/hs/entrydsm/application/integrate/admin/ApplicationExportRepository.java
+++ application/src/main/java/kr/hs/entrydsm/application/integrate/admin/ApplicationExportRepository.java
@@ -1,6 +1,6 @@
 package kr.hs.entrydsm.application.integrate.admin;
 
-import kr.hs.entrydsm.application.entity.Applicant;
+import kr.hs.entrydsm.application.entity.GraduationApplication;
 import kr.hs.entrydsm.common.context.beans.Published;
 import kr.hs.entrydsm.common.model.ReportCard;
 
@@ -8,6 +8,6 @@ import java.util.List;
 
 @Published
 public interface ApplicationExportRepository {
-
+    List<GraduationApplication> getApplicants();
     ReportCard getReportCard(long receiptCode);
 }
