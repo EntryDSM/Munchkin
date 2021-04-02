c5a25fe30740b7b9101341da8cd74556043205bf
leeseojune <sung07288346@gmail.com>
2021-04-02 17:18:34 +0900

    [UPDATE] Score부분 추가

--- application/src/main/java/kr/hs/entrydsm/application/integrate/score/ScoreCalculator.java
+++ application/src/main/java/kr/hs/entrydsm/application/integrate/score/ScoreCalculator.java
@@ -3,6 +3,10 @@ package kr.hs.entrydsm.application.integrate.score;
 import kr.hs.entrydsm.common.model.Scores;
 
 public interface ScoreCalculator {
+<<<<<<< HEAD
 
+=======
+    Iterable<Score> getAll();
+>>>>>>> 39160df... [UPDATE] Score부분 추가
     Scores getScores(Long receiptCode);
 }
