5cf23f1ab017f4bbe2780682011505f94d43a875
leeseojune <sung07288346@gmail.com>
2021-04-02 17:19:31 +0900

    [UPDATE] 오류 해결

--- application/src/main/java/kr/hs/entrydsm/application/integrate/score/ScoreCalculator.java
+++ application/src/main/java/kr/hs/entrydsm/application/integrate/score/ScoreCalculator.java
@@ -1,12 +1,9 @@
 package kr.hs.entrydsm.application.integrate.score;
 
+import kr.hs.entrydsm.application.usecase.dto.Score;
 import kr.hs.entrydsm.common.model.Scores;
 
 public interface ScoreCalculator {
-<<<<<<< HEAD
-
-=======
     Iterable<Score> getAll();
->>>>>>> 39160df... [UPDATE] Score부분 추가
     Scores getScores(Long receiptCode);
 }
