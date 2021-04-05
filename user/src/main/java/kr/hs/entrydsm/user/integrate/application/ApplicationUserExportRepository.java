afd76351ec98978b3aaf7ef2dc298dc93dc87801
leeseojune <sung07288346@gmail.com>
2021-04-05 20:17:53 +0900

    [FIX] 오류해결

--- user/src/main/java/kr/hs/entrydsm/user/integrate/application/ApplicationUserExportRepository.java
+++ user/src/main/java/kr/hs/entrydsm/user/integrate/application/ApplicationUserExportRepository.java
@@ -7,13 +7,8 @@ import java.time.LocalDate;
 
 @Published
 public interface ApplicationUserExportRepository {
-<<<<<<< HEAD
-    User findByReceiptCode(long receiptCode); // receiptCode를 이용해서 User를 찾는 함수
-    void changeApplication(long receiptCode, String educationalStatus, String applicationType, boolean isDaejeon, String applicationRemark); //
-=======
     User findByReceiptCode(long receiptCode);
     void changeApplication(long receiptCode, String educationalStatus, String applicationType, boolean isDaejeon, String applicationRemark);
     void changeInformation(long receiptCode, String name, String sex, LocalDate birthday, String parentName, String parentTel, String telephoneNumber, String homeTel, String address, String postCode, String photoFileName);
->>>>>>> 0c41cb1... [UPDATE] Applicant Information 추가 메소드
 
 }
