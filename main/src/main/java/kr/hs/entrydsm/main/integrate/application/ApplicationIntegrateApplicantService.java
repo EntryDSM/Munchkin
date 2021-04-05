6ccccbb82c5c8536949c0aa39509f964e2b5e794
LeagueLugas <leaguelugas@gmail.com>
2021-04-05 22:00:02 +0900

    [FIX] user integrate application class

--- main/src/main/java/kr/hs/entrydsm/main/integrate/application/ApplicationIntegrateApplicantService.java
+++ main/src/main/java/kr/hs/entrydsm/main/integrate/application/ApplicationIntegrateApplicantService.java
@@ -4,11 +4,17 @@ import kr.hs.entrydsm.application.integrate.user.ApplicantExportService;
 import kr.hs.entrydsm.application.usecase.dto.Application;
 import kr.hs.entrydsm.application.usecase.dto.Information;
 import kr.hs.entrydsm.common.context.beans.Published;
+import kr.hs.entrydsm.user.integrate.application.ApplicationUserExportManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Published
 @Service
 public class ApplicationIntegrateApplicantService implements ApplicantExportService {
+
+    private final ApplicationUserExportManager userExportManager;
+
     @Override
     public void writeApplicationType(Long receiptCode, Application application) {
 
@@ -16,7 +22,7 @@ public class ApplicationIntegrateApplicantService implements ApplicantExportServ
 
     @Override
     public void writeInformation(Long receiptCode, Information information) {
-        userExportRepository.changeInformation(receiptCode, information.getName(), information.getSex(),
+        userExportManager.changeInformation(receiptCode, information.getName(), information.getSex(),
                 information.getBirthday(), information.getParentName(), information.getParentTel(),
                 information.getTelephoneNumber(), information.getHomeTel(), information.getAddress(),
                 information.getPostCode(), information.getPhotoFileName());
