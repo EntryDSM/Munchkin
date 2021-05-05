55f4e6cc714e8b26ca61dbecbac278d252dfe5ec
leeseojune <sung07288346@gmail.com>
2021-05-05 22:45:55 +0900

    [ADD] BinaryUtils

---
+++ application/src/main/java/kr/hs/entrydsm/application/usecase/image/BinaryUtils.java
@@ -0,0 +1,34 @@
+package kr.hs.entrydsm.application.usecase.image;
+
+import java.util.Locale;
+
+public class BinaryUtils {
+    public static String toHex(byte[] data) {
+        StringBuilder sb = new StringBuilder(data.length * 2);
+        for (int i = 0; i < data.length; i++) {
+            String hex = Integer.toHexString(data[i]);
+            if (hex.length() == 1) {
+                // Append leading zero.
+                sb.append("0");
+            } else if (hex.length() == 8) {
+                // Remove ff prefix from negative numbers.
+                hex = hex.substring(6);
+            }
+            sb.append(hex);
+        }
+        return sb.toString().toLowerCase(Locale.getDefault());
+    }
+
+    public static byte[] fromHex(String hexData) {
+        byte[] result = new byte[(hexData.length() + 1) / 2];
+        String hexNumber = null;
+        int stringOffset = 0;
+        int byteOffset = 0;
+        while (stringOffset < hexData.length()) {
+            hexNumber = hexData.substring(stringOffset, stringOffset + 2);
+            stringOffset += 2;
+            result[byteOffset++] = (byte) Integer.parseInt(hexNumber, 16);
+        }
+        return result;
+    }
+}
