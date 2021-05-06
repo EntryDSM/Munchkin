47d6778881f0cbc57b86c4d2e339bbad2717d48e
이승윤 <yyuunn17@naver.com>
2021-05-06 15:43:28 +0900

    [ADD] @notblank 추가

--- admin/src/main/java/kr/hs/entrydsm/admin/usecase/dto/request/SignInRequest.java
+++ admin/src/main/java/kr/hs/entrydsm/admin/usecase/dto/request/SignInRequest.java
@@ -3,12 +3,16 @@ package kr.hs.entrydsm.admin.usecase.dto.request;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Builder
 public class SignInRequest {
 
+    @NotBlank
     private String id;
 
+    @NotBlank
     private String password;
 
 }
