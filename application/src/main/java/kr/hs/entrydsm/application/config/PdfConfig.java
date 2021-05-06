09f35955c6550e00d9a9d4599bbb36e983753b19
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-06 11:28:01 +0900

    [UPDATE] 인라인 Font 클래스 추출

--- application/src/main/java/kr/hs/entrydsm/application/config/PdfConfig.java
+++ application/src/main/java/kr/hs/entrydsm/application/config/PdfConfig.java
@@ -17,13 +17,6 @@ import java.util.List;
 @Configuration
 public class PdfConfig {
 
-    private static class Font {
-        public static String KO_PUB_WORLD_DOTUM_LIGHT = "KO_PUB_WORLD_DOTUM_LIGHT";
-        public static String KO_PUB_WORLD_DOTUM_BOLD = "KO_PUB_WORLD_DOTUM_BOLD";
-        public static String KO_PUB_WORLD_DOTUM_MEDIUM = "KO_PUB_WORLD_DOTUM_MEDIUM";
-        public static String DEJA_VU_SANS = "DEJA_VU_SANS";
-    }
-
     @Bean
     public TemplateEngine templateEngine() {
         ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
