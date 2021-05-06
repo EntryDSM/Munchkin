7e09c08ceda3aa8a0f6fa939913702ecf628fe42
이승윤 <yyuunn17@naver.com>
2021-05-06 14:58:58 +0900

    [ADD] getApplicantScore

--- score/src/main/java/kr/hs/entrydsm/score/integrate/admin/ScoreExportManager.java
+++ score/src/main/java/kr/hs/entrydsm/score/integrate/admin/ScoreExportManager.java
@@ -4,6 +4,7 @@ import kr.hs.entrydsm.score.entity.Score;
 import kr.hs.entrydsm.score.entity.ScoreRepository;
 import kr.hs.entrydsm.score.integrate.user.ScorerRepository;
 import kr.hs.entrydsm.score.integrate.user.enumeration.ApplicationType;
+import kr.hs.entrydsm.score.usecase.dto.ApplicantScore;
 import kr.hs.entrydsm.score.usecase.dto.ApplicationStatusResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -27,6 +28,11 @@ public class ScoreExportManager implements ScoreExportRepository{
                 .meisterScore(getScores(ApplicationType.MEISTER))
                 .specialScore(getScores(ApplicationType.SOCIAL))
                 .build();
+    }
+
+    @Override
+    public ApplicantScore getApplicantScore(Long receiptCode) {
+        return null;
     }
 
     private List<BigDecimal> getScores(ApplicationType applicationType) {
--- score/src/main/java/kr/hs/entrydsm/score/integrate/admin/ScoreExportRepository.java
+++ score/src/main/java/kr/hs/entrydsm/score/integrate/admin/ScoreExportRepository.java
@@ -1,7 +1,9 @@
 package kr.hs.entrydsm.score.integrate.admin;
 
+import kr.hs.entrydsm.score.usecase.dto.ApplicantScore;
 import kr.hs.entrydsm.score.usecase.dto.ApplicationStatusResponse;
 
 public interface ScoreExportRepository {
     ApplicationStatusResponse getApplicationStatus();
+    ApplicantScore getApplicantScore(Long receiptCode);
 }
