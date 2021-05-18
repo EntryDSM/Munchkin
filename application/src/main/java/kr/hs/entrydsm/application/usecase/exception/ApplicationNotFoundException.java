bcea6570a67af313528ce35d70d389ed59956f1c
leeseojune <sung07288346@gmail.com>
2021-05-18 15:48:12 +0900

    [UPDATE] File Is Empty 추가

--- application/src/main/java/kr/hs/entrydsm/application/usecase/exception/ApplicationNotFoundException.java
+++ application/src/main/java/kr/hs/entrydsm/application/usecase/exception/ApplicationNotFoundException.java
@@ -8,4 +8,5 @@ public class ApplicationNotFoundException extends MunchkinException {
     public ApplicationNotFoundException() {
         super(ErrorCode.APPLICATION_NOT_FOUND);
     }
+
 }
--- common/src/main/java/kr/hs/entrydsm/common/context/exception/ErrorCode.java
+++ common/src/main/java/kr/hs/entrydsm/common/context/exception/ErrorCode.java
@@ -27,7 +27,8 @@ public enum ErrorCode {
     NOT_AUTHORIZED(401, "NOTIFICATION401-0","Check the authority"),
 
     APPLICATION_NOT_FOUND(404, "APPLICATION404-0","The application could not be found"),
-    SCHOOL_NOT_FOUND(404, "APPLICATION404-1", "The school could not be found");
+    SCHOOL_NOT_FOUND(404, "APPLICATION404-1", "The school could not be found"),
+    FILE_IS_EMPTY(400, "APPLICATION404-2", "The file could not be found");
 
     private final int status;
     private final String code;
