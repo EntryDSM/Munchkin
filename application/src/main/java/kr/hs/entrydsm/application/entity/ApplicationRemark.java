a33b147df2b21262c2768e78ff95de235a09757d
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-20 15:51:28 +0900

    [ADD] ApplicationRemark

---
+++ application/src/main/java/kr/hs/entrydsm/application/entity/ApplicationRemark.java
@@ -0,0 +1,11 @@
+package kr.hs.entrydsm.application.entity;
+
+public class ApplicationRemark {
+
+    public final static String ONE_PARENT = "ONE_PARENT";
+    public final static String FROM_NORTH = "FROM_NORTH";
+    public final static String MULTICULTURAL = "MULTICULTURAL";
+    public final static String BASIC_LIVING = "BASIC_LIVING";
+    public final static String LOWEST_INCOME = "LOWEST_INCOME";
+    public final static String TEEN_HOUSEHOLDER = "TEEN_HOUSEHOLDER";
+}
