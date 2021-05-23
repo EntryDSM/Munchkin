efd1eff51a3d991580b8fc4de654f33c388a1e03
LeagueLugas <leaguelugas@gmail.com>
2021-05-23 21:40:57 +0900

    [ADD] Async Config

---
+++ main/src/main/java/kr/hs/entrydsm/main/security/async/AsyncConfig.java
@@ -0,0 +1,26 @@
+package kr.hs.entrydsm.main.security.async;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("Munchkin-Async-");
+        executor.initialize();
+        return executor;
+    }
+
+}
