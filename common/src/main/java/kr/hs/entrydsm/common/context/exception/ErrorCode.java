260264c01340b691386ff3db61e6ed7b927c20ba
이승윤 <yyuunn17@naver.com>
2021-06-04 17:05:37 +0900

    [UPDATE] not accessible exception 401->403
    토큰이 있어도 권한 자체가 다른 것이기 때문에 403 반환하는 것으로 수정함

--- common/src/main/java/kr/hs/entrydsm/common/context/exception/ErrorCode.java
+++ common/src/main/java/kr/hs/entrydsm/common/context/exception/ErrorCode.java
@@ -20,7 +20,7 @@ public enum ErrorCode {
     EXPIRED_TOKEN(401, "ADMIN401-1","Token is Expired"),
     ADMIN_NOT_FOUND(404, "ADMIN404-0","The account does not exist"),
     APPLICANT_NOT_FOUND(404, "ADMIN404-1","The applicant does not exist"),
-    NOT_ACCESSIBLE(401, "ADMIN401-2","Check the token"),
+    NOT_ACCESSIBLE(403, "ADMIN401-2","Check the token"),
     ALREADY_EXIST_ADMIN_ID(409, "ADMIN409-0", "Already admin id exist"),
     NOT_FINAL_SUBMITTED(423,"ADMIN423-0", "Application not final submitted"),
 
