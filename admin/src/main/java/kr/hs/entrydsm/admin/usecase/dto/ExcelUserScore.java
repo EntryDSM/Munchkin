57126b7f3e4b85a997df5c9d560cc84581672e1c
이승윤 <yyuunn17@naver.com>
2021-06-08 22:08:17 +0900

    [ADD] @NoArgs_ @AllArgs_ 어노테이션

--- admin/src/main/java/kr/hs/entrydsm/admin/usecase/dto/ExcelUserScore.java
+++ admin/src/main/java/kr/hs/entrydsm/admin/usecase/dto/ExcelUserScore.java
@@ -1,10 +1,14 @@
 package kr.hs.entrydsm.admin.usecase.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ExcelUserScore {
 
     private String yearOfGraduation; //졸업년도
--- admin/src/main/java/kr/hs/entrydsm/admin/usecase/dto/Schedules.java
+++ admin/src/main/java/kr/hs/entrydsm/admin/usecase/dto/Schedules.java
@@ -1,13 +1,17 @@
 package kr.hs.entrydsm.admin.usecase.dto;
 
 import kr.hs.entrydsm.admin.entity.schedule.Type;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Schedules {
 
     private String year;
