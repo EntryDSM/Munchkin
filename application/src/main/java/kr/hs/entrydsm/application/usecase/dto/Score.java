046cd64d61b2708c1bffb912a732a9ba32c1c602
leeseojune <sung07288346@gmail.com>
2021-06-08 23:34:43 +0900

    [FIX] 자료형 변경

--- application/src/main/java/kr/hs/entrydsm/application/usecase/dto/Score.java
+++ application/src/main/java/kr/hs/entrydsm/application/usecase/dto/Score.java
@@ -5,17 +5,19 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Builder
 public class Score {
     private Long receiptCode;
-    private String totalFirstGradeScores; //1학년 성적 총합
-    private String totalSecondGradeScores; //2학년 성적 총합
-    private String totalThirdGradeScores; //3학년 성적 총합
-    private String volunteerScore; //봉사점수 총합
-    private String conversionScore; //교과 성적 환산 점수
-    private String attendanceScore; //출석 점수
-    private String totalScoreFirstRound; //1차 전형 총점
+    private BigDecimal totalFirstGradeScores; //1학년 성적 총합
+    private BigDecimal totalSecondGradeScores; //2학년 성적 총합
+    private BigDecimal totalThirdGradeScores; //3학년 성적 총합
+    private int volunteerScore; //봉사점수 총합
+    private BigDecimal conversionScore; //교과 성적 환산 점수
+    private BigDecimal attendanceScore; //출석 점수
+    private BigDecimal totalScoreFirstRound; //1차 전형 총점
 }
