f9be2ad537b9cd81769a398fdde001501e1bda85
김대웅 <dkssud9556@dsm.hs.kr>
2021-06-09 22:38:45 +0900

    [FIX] ScoreCalculator.getScore 분리

--- application/src/main/java/kr/hs/entrydsm/application/integrate/score/ScoreCalculator.java
+++ application/src/main/java/kr/hs/entrydsm/application/integrate/score/ScoreCalculator.java
@@ -1,9 +1,12 @@
 package kr.hs.entrydsm.application.integrate.score;
 
 import kr.hs.entrydsm.application.entity.Application;
+import kr.hs.entrydsm.application.entity.GraduationApplication;
+import kr.hs.entrydsm.application.entity.QualificationExamApplication;
 import kr.hs.entrydsm.application.usecase.dto.Score;
 
 public interface ScoreCalculator {
     Iterable<Score> getAll();
-    Score getScore(Application application);
+    Score getGraduationScore(GraduationApplication application);
+    Score getQualificationExamScore(QualificationExamApplication application);
 }
