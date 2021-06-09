9d446eb275dc97a1538ef598bb595f43326c8d70
LeagueLugas <leaguelugas@gmail.com>
2021-06-09 19:27:57 +0900

    [ADD] aws ses access key in property

--- common/src/main/java/kr/hs/entrydsm/common/context/sender/ses/AwsSesConfiguration.java
+++ common/src/main/java/kr/hs/entrydsm/common/context/sender/ses/AwsSesConfiguration.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AwsSesConfiguration {
 
-    @Value("${aws.ses.access_key}")
+    @Value("${aws.ses.access-key}")
     private String AWS_ACCESS_KEY_ID;
 
     @Value("${aws.ses.secret_key}")
