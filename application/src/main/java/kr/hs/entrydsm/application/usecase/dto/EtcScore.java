64410353b7552bccc39147fd1251d9befa160684
김대웅 <dkssud9556@dsm.hs.kr>
2021-06-10 10:18:39 +0900

    [UPDATE] TotalGrade에 EtcScore 넣음

--- application/src/main/java/kr/hs/entrydsm/application/usecase/dto/EtcScore.java
+++ application/src/main/java/kr/hs/entrydsm/application/usecase/dto/EtcScore.java
@@ -1,6 +1,8 @@
 package kr.hs.entrydsm.application.usecase.dto;
 
+import kr.hs.entrydsm.application.entity.GraduationApplication;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +11,7 @@ import javax.validation.constraints.NotNull;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class EtcScore {
 
     @NotNull
@@ -25,5 +28,15 @@ public class EtcScore {
 
     @NotNull
     private Integer earlyLeaveCount;
+
+    public static EtcScore from(GraduationApplication application) {
+        return EtcScore.builder()
+                .volunteerTime(application.getVolunteerTime())
+                .dayAbsenceCount(application.getDayAbsenceCount())
+                .lectureAbsenceCount(application.getLectureAbsenceCount())
+                .latenessCount(application.getLatenessCount())
+                .earlyLeaveCount(application.getEarlyLeaveCount())
+                .build();
+    }
 
 }
--- application/src/main/java/kr/hs/entrydsm/application/usecase/dto/TotalGrade.java
+++ application/src/main/java/kr/hs/entrydsm/application/usecase/dto/TotalGrade.java
@@ -1,13 +1,10 @@
 package kr.hs.entrydsm.application.usecase.dto;
 
-import kr.hs.entrydsm.application.entity.GraduationApplication;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PositiveOrZero;
 
 @AllArgsConstructor
 @Builder
@@ -17,29 +14,13 @@ public class TotalGrade {
     @NotNull
     private final SubjectScore subjectScore;
 
-    @PositiveOrZero
-    private final int volunteerTime;
+    @NotNull
+    private final EtcScore etcScore;
 
-    @PositiveOrZero
-    private final int dayAbsenceCount;
-
-    @PositiveOrZero
-    private final int lectureAbsenceCount;
-
-    @PositiveOrZero
-    private final int latenessCount;
-
-    @PositiveOrZero
-    private final int earlyLeaveCount;
-
-    public static TotalGrade fromApplicationAndSubjectScore(GraduationApplication application, SubjectScore subjectScore) {
+    public static TotalGrade from(SubjectScore subjectScore, EtcScore etcScore) {
         return TotalGrade.builder()
                 .subjectScore(subjectScore)
-                .dayAbsenceCount(application.getDayAbsenceCount())
-                .earlyLeaveCount(application.getEarlyLeaveCount())
-                .latenessCount(application.getLatenessCount())
-                .lectureAbsenceCount(application.getLectureAbsenceCount())
-                .volunteerTime(application.getVolunteerTime())
+                .etcScore(etcScore)
                 .build();
     }
 }
