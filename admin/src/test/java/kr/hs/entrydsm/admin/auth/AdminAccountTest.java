c5db1b14850b18b2133a01db14df58eb47a479b9
이승윤 <yyuunn17@naver.com>
2021-06-10 17:20:12 +0900

    [ADD] 어드민 계정 관련 테스트

---
+++ admin/src/test/java/kr/hs/entrydsm/admin/auth/AdminAccountTest.java
@@ -0,0 +1,89 @@
+package kr.hs.entrydsm.admin.auth;
+
+import kr.hs.entrydsm.admin.entity.admin.Admin;
+import kr.hs.entrydsm.admin.entity.admin.Permission;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("어드민 계정 테스트")
+public class AdminAccountTest extends AdminTest {
+
+    @Test
+    public void 어드민_선생님_계정_추가() {
+        assertTrue(addAccount(
+                Admin.builder()
+                        .id("test1234")
+                        .name("교무실")
+                        .password("testPassword")
+                        .permission(Permission.TEACHER)
+                        .build()
+                )
+        );
+    }
+
+    @Test
+    public void 어드민_선생님_계정_추가_실패() {
+        assertFalse(addAccount(
+                Admin.builder()
+                        .id("test2@test.com")
+                        .name("교무실")
+                        .password("testPassword")
+                        .permission(Permission.TEACHER)
+                        .build()
+                )
+        );
+    }
+
+    @Test
+    public void 어드민_행정실_계정_추가() {
+        assertTrue(addAccount(
+                Admin.builder()
+                        .id("test4567")
+                        .name("행정실")
+                        .password("testPassword")
+                        .permission(Permission.OFFICE)
+                        .build()
+                )
+        );
+    }
+
+    @Test
+    public void 어드민_행정실_계정_추가_실패() {
+        assertFalse(addAccount(
+                Admin.builder()
+                        .id("test4567890")
+                        .name("행정실")
+                        .password("testPassword")
+                        .permission(Permission.OFFICE)
+                        .build()
+                )
+        );
+    }
+
+    @Test
+    public void 어드민_교무실_로그인() {
+        assertEquals(TEACHER_ADMIN.getId(), "asdf1234");
+        assertEquals(TEACHER_ADMIN.getPassword(), "teacheradmin");
+    }
+
+    @Test
+    public void 어드민_교무실_로그인_실패() {
+        assertNotEquals(TEACHER_ADMIN.getId(), "asdf12345");
+        assertNotEquals(TEACHER_ADMIN.getPassword(), "teachderadmin");
+    }
+
+    @Test
+    public void 어드민_행정실_로그인() {
+        assertEquals(OFFICE_ADMIN.getId(), "asdf4567");
+        assertEquals(OFFICE_ADMIN.getPassword(), "officeadmin");
+    }
+
+    @Test
+    public void 어드민_행정실_로그인_실패() {
+        assertNotEquals(OFFICE_ADMIN.getId(), "asdf4568");
+        assertNotEquals(OFFICE_ADMIN.getPassword(), "offdiceadmin");
+    }
+
+}
---
+++ admin/src/test/java/kr/hs/entrydsm/admin/auth/AdminTest.java
@@ -0,0 +1,38 @@
+package kr.hs.entrydsm.admin.auth;
+
+import kr.hs.entrydsm.admin.entity.admin.Admin;
+import kr.hs.entrydsm.admin.entity.admin.Permission;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdminTest {
+
+    protected static final Admin TEACHER_ADMIN = Admin.builder()
+            .id("asdf1234")
+            .password("teacheradmin")
+            .name("본부교무실")
+            .permission(Permission.TEACHER)
+            .build();
+
+    protected static final Admin OFFICE_ADMIN = Admin.builder()
+            .id("asdf4567")
+            .password("officeadmin")
+            .name("행정실")
+            .permission(Permission.OFFICE)
+            .build();
+
+    private static final List<Admin> ADMINS = new ArrayList<>();
+
+    protected static boolean addAccount(Admin admin) {
+        boolean exists = ADMINS.stream().anyMatch(u -> u.getId().equals(admin.getId()));
+        boolean requestCheck = admin.getId().length() > 8 || admin.getName().length() > 5;
+
+        if (exists) return false;
+        if(requestCheck) return false;
+
+        ADMINS.add(admin);
+        return true;
+    }
+
+}
