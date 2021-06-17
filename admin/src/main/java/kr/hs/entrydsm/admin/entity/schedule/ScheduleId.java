a29dd66c182de5d5e80924c07ddde77fccebded5
이승윤 <yyuunn17@naver.com>
2021-06-17 23:39:21 +0900

    [UPDATE] @Enumerated(EnumType.STRING)

--- admin/src/main/java/kr/hs/entrydsm/admin/entity/schedule/ScheduleId.java
+++ admin/src/main/java/kr/hs/entrydsm/admin/entity/schedule/ScheduleId.java
@@ -2,6 +2,8 @@ package kr.hs.entrydsm.admin.entity.schedule;
 
 import lombok.NoArgsConstructor;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import java.io.Serializable;
 
@@ -11,6 +13,7 @@ public class ScheduleId implements Serializable {
     @Id
     private String year;
 
+    @Enumerated(EnumType.STRING)
     @Id
     private Type type;
 
