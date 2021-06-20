f8f6badef6a0e96a82e2f583adc224d8c0c43ce0
김대웅 <dkssud9556@dsm.hs.kr>
2021-06-20 21:42:13 +0900

    [FIX] dto 에러

--- application/src/main/java/kr/hs/entrydsm/application/usecase/dto/UpdateDocsRequest.java
+++ application/src/main/java/kr/hs/entrydsm/application/usecase/dto/UpdateDocsRequest.java
@@ -1,11 +1,15 @@
 package kr.hs.entrydsm.application.usecase.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Getter
+@NoArgsConstructor
+@Builder
 public class UpdateDocsRequest {
 
-    private final String content;
+    private String content;
 }
