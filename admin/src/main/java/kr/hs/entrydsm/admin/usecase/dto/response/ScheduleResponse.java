ccd9efefa37b17446b1fbba4439e08a60a461b48
이승윤 <yyuunn17@naver.com>
2021-06-20 20:21:27 +0900

    [ADD] getCurrentStatus

--- admin/src/main/java/kr/hs/entrydsm/admin/usecase/dto/response/ScheduleResponse.java
+++ admin/src/main/java/kr/hs/entrydsm/admin/usecase/dto/response/ScheduleResponse.java
@@ -3,13 +3,17 @@ package kr.hs.entrydsm.admin.usecase.dto.response;
 import kr.hs.entrydsm.admin.usecase.dto.Schedules;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @Builder
+@NoArgsConstructor
 public class ScheduleResponse {
 
-    List<Schedules> schedules;
+    private List<Schedules> schedules;
+
+    private String currentStatus;
 
 }
--- admin/src/main/java/kr/hs/entrydsm/admin/usecase/schedule/ScheduleServiceManager.java
+++ admin/src/main/java/kr/hs/entrydsm/admin/usecase/schedule/ScheduleServiceManager.java
@@ -5,6 +5,7 @@ import kr.hs.entrydsm.admin.entity.admin.AdminRepository;
 import kr.hs.entrydsm.admin.entity.admin.Permission;
 import kr.hs.entrydsm.admin.entity.schedule.Schedule;
 import kr.hs.entrydsm.admin.entity.schedule.ScheduleRepository;
+import kr.hs.entrydsm.admin.entity.schedule.Type;
 import kr.hs.entrydsm.admin.infrastructure.database.ScheduleRepositoryManager;
 import kr.hs.entrydsm.admin.usecase.dto.Schedules;
 import kr.hs.entrydsm.admin.usecase.dto.request.ScheduleRequest;
@@ -15,6 +16,7 @@ import kr.hs.entrydsm.common.context.auth.manager.AuthenticationManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -60,7 +62,39 @@ public class ScheduleServiceManager implements ScheduleService {
 
         return ScheduleResponse.builder()
                 .schedules(schedules)
+                .currentStatus(getCurrentStatus())
                 .build();
+    }
+
+    private String getCurrentStatus() {
+        LocalDate now = LocalDate.now();
+        String year = String.valueOf(now.getYear());
+
+        Schedule startDate = scheduleRepository.findByYearAndType(year, Type.START_DATE);
+        Schedule endDate = scheduleRepository.findByYearAndType(year, Type.END_DATE);
+        Schedule firstAnnounce = scheduleRepository.findByYearAndType(year, Type.FIRST_ANNOUNCEMENT);
+        Schedule secondAnnounce = scheduleRepository.findByYearAndType(year, Type.SECOND_ANNOUNCEMENT);
+        Schedule interview = scheduleRepository.findByYearAndType(year, Type.INTERVIEW);
+
+        if(now.isBefore(startDate.getDate())) {
+            return "NOT_APPLICATION_PERIOD";
+        } else if(!now.isAfter(endDate.getDate())) {
+            return startDate.getType().toString(); //START_DATE
+        } else if(now.isBefore(firstAnnounce.getDate())) {
+            return "BEFORE_FIRST_ANNOUNCE"; //서류 합격 발표 전
+        } else if(now.isEqual(firstAnnounce.getDate())) {
+            return firstAnnounce.getType().toString();
+        } else if(now.isBefore(interview.getDate())) {
+            return "BEFORE_INTERVIEW";
+        } else if(now.isEqual(interview.getDate())) {
+            return interview.getType().toString();
+        } else if(now.isBefore(secondAnnounce.getDate())) {
+            return "BEFORE_SECOND_ANNOUNCE";
+        } else if(now.isEqual(secondAnnounce.getDate())) {
+            return secondAnnounce.getType().toString();
+        } else {
+            return "END";
+        }
     }
 
 }
