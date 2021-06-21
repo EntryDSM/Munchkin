4ee09c3d824cf0a68fd434c6bbec010c23051d9b
김대웅 <dkssud9556@dsm.hs.kr>
2021-06-21 23:32:50 +0900

    [UPDATE] update docs dto validation

--- application/src/main/java/kr/hs/entrydsm/application/usecase/dto/UpdateDocsRequest.java
+++ application/src/main/java/kr/hs/entrydsm/application/usecase/dto/UpdateDocsRequest.java
@@ -5,11 +5,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Size;
+
 @AllArgsConstructor
 @Getter
 @NoArgsConstructor
 @Builder
 public class UpdateDocsRequest {
 
+    @Size(max = 1600)
     private String content;
 }
