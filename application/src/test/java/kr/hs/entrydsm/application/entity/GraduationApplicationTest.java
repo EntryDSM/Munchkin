ea047161be28be88e3e14c244ddece320716895d
김대웅 <dkssud9556@dsm.hs.kr>
2021-06-22 15:36:55 +0900

    [TEST] test 코드 추가

--- application/src/test/java/kr/hs/entrydsm/application/entity/GraduationApplicationTest.java
+++ application/src/test/java/kr/hs/entrydsm/application/entity/GraduationApplicationTest.java
@@ -35,4 +35,31 @@ public class GraduationApplicationTest {
         assertThat(application.getTechAndHomeScore()).isEqualTo("AXAAAA");
         assertThat(application.getGraduateAt()).isEqualTo(LocalDate.of(2019, 2, 1));
     }
+
+    @Test
+    public void getSchoolInfo() {
+        GraduationApplication application = GraduationApplicationBuilder.build();
+
+        assertThat(application.getSchoolName()).isEqualTo("대전하기중학교");
+        assertThat(application.getSchoolCode()).isEqualTo("33333");
+        assertThat(application.getSchoolClass()).isEqualTo("4");
+    }
+
+    @Test
+    public void setSchoolInfo() {
+        GraduationApplication application = GraduationApplicationBuilder.build();
+        School school = School.builder()
+                .name("대덕소프트웨어마이스터중학교")
+                .address("대전광역시 유성구 장동")
+                .code("44444")
+                .information("대전광역시 유성구 장동 대덕대학교 옆")
+                .build();
+
+        application.setSchoolTel("0421111111");
+        application.setSchool(school);
+
+        assertThat(application.getSchoolTel()).isEqualTo("0421111111");
+        assertThat(application.getSchoolName()).isEqualTo("대덕소프트웨어마이스터중학교");
+        assertThat(application.getSchoolCode()).isEqualTo("44444");
+    }
 }
