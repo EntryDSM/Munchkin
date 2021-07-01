38de67c3a096f9cf85b0c0dc3e8771bd6f7e47ae
김대웅 <dkssud9556@dsm.hs.kr>
2021-07-01 17:10:07 +0900

    [ADD] ApplicationFactory

---
+++ application/src/main/java/kr/hs/entrydsm/application/ApplicationFactory.java
@@ -0,0 +1,44 @@
+package kr.hs.entrydsm.application;
+
+import kr.hs.entrydsm.application.entity.*;
+import kr.hs.entrydsm.application.usecase.exception.ApplicationNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ApplicationFactory {
+
+    private final GraduationApplicationRepository graduationApplicationRepository;
+    private final QualificationExamApplicationRepository qualificationExamApplicationRepository;
+
+    public GraduationApplication saveGraduationApplicationIfNotExists(long receiptCode) {
+        GraduationApplication graduationApplication = createGraduationApplicationIfNotExists(receiptCode);
+        graduationApplicationRepository.save(graduationApplication);
+        return graduationApplication;
+    }
+
+    public QualificationExamApplication saveQualificationExamApplicationIfNotExists(long receiptCode) {
+        QualificationExamApplication qualificationExamApplication = createQualificationExamApplicationIfNotExists(receiptCode);
+        qualificationExamApplicationRepository.save(qualificationExamApplication);
+        return qualificationExamApplication;
+    }
+
+    public GraduationApplication createGraduationApplicationIfNotExists(long receiptCode) {
+        if (graduationApplicationRepository.existsByReceiptCode(receiptCode)) {
+            return graduationApplicationRepository.findByReceiptCode(receiptCode)
+                    .orElseThrow(ApplicationNotFoundException::new);
+        } else {
+            return new GraduationApplication(receiptCode);
+        }
+    }
+
+    public QualificationExamApplication createQualificationExamApplicationIfNotExists(long receiptCode) {
+        if (qualificationExamApplicationRepository.existsByReceiptCode(receiptCode)) {
+            return qualificationExamApplicationRepository.findByReceiptCode(receiptCode)
+                    .orElseThrow(ApplicationNotFoundException::new);
+        } else {
+            return new QualificationExamApplication(receiptCode);
+        }
+    }
+}
