ba45d10f4fc099b726f60946ee43a9f3271c8ffc
leeseojune <sung07288346@gmail.com>
2021-07-02 11:43:07 +0900

    [UPDATE] @Repositroy

--- application/src/main/java/kr/hs/entrydsm/application/infrastructure/database/QualificationExamApplicationRepositoryManager.java
+++ application/src/main/java/kr/hs/entrydsm/application/infrastructure/database/QualificationExamApplicationRepositoryManager.java
@@ -3,6 +3,8 @@ package kr.hs.entrydsm.application.infrastructure.database;
 import kr.hs.entrydsm.application.entity.QualificationExamApplication;
 import kr.hs.entrydsm.application.entity.QualificationExamApplicationRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QualificationExamApplicationRepositoryManager extends QualificationExamApplicationRepository, CrudRepository<QualificationExamApplication, Long> {
 }
