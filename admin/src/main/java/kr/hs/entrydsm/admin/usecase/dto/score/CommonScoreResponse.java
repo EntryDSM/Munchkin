c592a052b86dbff30013c66af86a412790525fed
이승윤 <yyuunn17@naver.com>
2021-07-09 09:32:53 +0900

    [UPDATE] dto 수정

--- admin/src/main/java/kr/hs/entrydsm/admin/usecase/dto/score/CommonScoreResponse.java
+++ admin/src/main/java/kr/hs/entrydsm/admin/usecase/dto/score/CommonScoreResponse.java
@@ -9,43 +9,43 @@ import lombok.*;
 @AllArgsConstructor
 public class CommonScoreResponse {
 
-    @JsonProperty(value = "141-150")
-    private int score141_150;
+    @JsonProperty(value = "158-170")
+    private int score158_170;
 
-    @JsonProperty(value = "131-140")
-    private int score131_140;
+    @JsonProperty(value = "145-157")
+    private int score145_157;
 
-    @JsonProperty(value = "121-130")
-    private int score121_130;
+    @JsonProperty(value = "132-144")
+    private int score132_144;
 
-    @JsonProperty(value = "111-120")
-    private int score111_120;
+    @JsonProperty(value = "119-131")
+    private int score119_131;
 
-    @JsonProperty(value = "101-110")
-    private int score101_110;
+    @JsonProperty(value = "106-118")
+    private int score106_118;
 
-    @JsonProperty(value = "91-100")
-    private int score91_100;
+    @JsonProperty(value = "93-105")
+    private int score93_105;
 
-    @JsonProperty(value = "81-90")
-    private int score81_90;
+    @JsonProperty(value = "80-92")
+    private int score80_92;
 
-    @JsonProperty(value = "-80")
-    private int score_80;
+    @JsonProperty(value = "-79")
+    private int score_79;
 
     private Integer commonCount;
 
     private Double commonCompetitionRate;
 
     public void addScore(double score) {
-        if (score <= 80) score_80++;
-        else if (score <= 90) score81_90++;
-        else if (score <= 100) score91_100++;
-        else if (score <= 110) score101_110++;
-        else if (score <= 120) score111_120++;
-        else if (score <= 130) score121_130++;
-        else if (score <= 140) score131_140++;
-        else if (score <= 150) score141_150++;
+        if(score <= 79) score_79++;
+        else if (score <= 92) score80_92++;
+        else if (score <= 105) score93_105++;
+        else if (score <= 118) score106_118++;
+        else if (score <= 131) score119_131++;
+        else if (score <= 144) score132_144++;
+        else if (score <= 157) score145_157++;
+        else if (score <= 170) score158_170++;
     }
 
     public void updateCountAndRate(Integer count, Double competitionRate) {
