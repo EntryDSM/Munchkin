6203f11dc0781be1bc0db2237d4157cf4b7b01d3
leeseojune <sung07288346@gmail.com>
2021-07-09 10:41:19 +0900

    [UPDATE] 쿠키..

--- user/src/main/java/kr/hs/entrydsm/user/usecase/UserServiceManager.java
+++ user/src/main/java/kr/hs/entrydsm/user/usecase/UserServiceManager.java
@@ -193,7 +193,7 @@ public class UserServiceManager implements UserAuthService, UserService {
         String refreshToken = tokenProvider.generateRefreshToken(receiptCode);
         HttpHeaders headers = new HttpHeaders();
         headers.set("Set-Cookie",
-                String.format("refresh-token=%s; Expires=%s; HttpOnly; Path=/; Domain=hs.kr", refreshToken, getExpireDateByString()));
+                String.format("refresh-token=%s; Expires=%s; HttpOnly; Path=/; Domain=entrydsm.hs.kr", refreshToken, getExpireDateByString()));
 
         refreshTokenRepository.findById(receiptCode)
                 .or(() -> Optional.of(new RefreshToken(receiptCode, refreshToken, refreshTokenExpiration)))
