a1fc0825b24d7592eddc15e5e0aa8658bdd42408
김대웅 <dkssud9556@dsm.hs.kr>
2021-07-09 09:46:30 +0900

    [FIX] ApplicationExportAdminManager

--- application/src/main/java/kr/hs/entrydsm/application/integrate/admin/ApplicationExportAdminManager.java
+++ application/src/main/java/kr/hs/entrydsm/application/integrate/admin/ApplicationExportAdminManager.java
@@ -7,6 +7,7 @@ import kr.hs.entrydsm.application.entity.GraduationApplication;
 import kr.hs.entrydsm.application.entity.GraduationApplicationRepository;
 import kr.hs.entrydsm.application.integrate.score.ScoreCalculator;
 import kr.hs.entrydsm.application.integrate.user.ApplicantRepository;
+import kr.hs.entrydsm.application.integrate.user.ApplicantStatusService;
 import kr.hs.entrydsm.application.usecase.dto.CalculatedScore;
 import kr.hs.entrydsm.application.usecase.dto.MiddleSchoolInfo;
 import kr.hs.entrydsm.application.usecase.dto.ReportCard;
@@ -21,21 +22,19 @@ public class ApplicationExportAdminManager implements ApplicationExportAdminRepo
 
     private final ApplicationFactory applicationFactory;
     private final ApplicantRepository applicantRepository;
+    private final ApplicantStatusService applicantStatusService;
     private final GraduationApplicationRepository graduationApplicationRepository;
     private final ScoreCalculator scoreCalculator;
 
     @Override
     public ReportCard getReportCard(long receiptCode) {
         Applicant applicant = applicantRepository.findByReceiptCode(receiptCode);
-        if (applicant.getEducationalStatus() == null)
-            throw new EducationalStatusNullException();
-
         Application application = applicationFactory.saveAndGetApplicationFrom(applicant);
 
-        if (!scoreCalculator.isExists(receiptCode))
-            throw new ScoreNotFoundException();
+        CalculatedScore calculatedScore = CalculatedScore.emptyScore(receiptCode);
+        if (applicantStatusService.isFinalSubmit(receiptCode))
+            calculatedScore = scoreCalculator.calculateScore(application);
 
-        CalculatedScore calculatedScore = scoreCalculator.calculateScore(application);
         return ReportCard.from(application, calculatedScore);
     }
 
