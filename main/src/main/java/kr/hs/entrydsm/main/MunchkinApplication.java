b14c912fcccddbb056b79f8bae840d21fff72fdf
김대웅 <dkssud9556@dsm.hs.kr>
2021-07-15 11:35:22 +0900

    [FIX] @EnableJpaAuditing 위치 변경

--- main/src/main/java/kr/hs/entrydsm/main/MunchkinApplication.java
+++ main/src/main/java/kr/hs/entrydsm/main/MunchkinApplication.java
@@ -2,9 +2,7 @@ package kr.hs.entrydsm.main;
 
 import kr.hs.entrydsm.main.configuration.MunchkinApplicationBuilder;
 import org.springframework.boot.SpringApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 public class MunchkinApplication {
 
     public static final String PROPS_CONFIG_NAME = "spring.config.name: main, user, admin, application, score";
