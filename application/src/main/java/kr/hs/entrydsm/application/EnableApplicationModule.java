3f578cb7e95ad42b0d6f07ba1f5e54bacfd3018a
Kim Daewoong <daewoong.kim@knowre.com>
2021-07-23 00:23:34 +0900

    [FIX] @EnableJpaAuditing 위치 변경

--- application/src/main/java/kr/hs/entrydsm/application/EnableApplicationModule.java
+++ application/src/main/java/kr/hs/entrydsm/application/EnableApplicationModule.java
@@ -2,7 +2,6 @@ package kr.hs.entrydsm.application;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.lang.annotation.*;
 
@@ -11,6 +10,5 @@ import java.lang.annotation.*;
 @Documented
 @ComponentScan(basePackageClasses = ApplicationModuleConfiguration.class)
 @Import(ApplicationModuleConfiguration.class)
-@EnableJpaAuditing
 public @interface EnableApplicationModule {
 }
