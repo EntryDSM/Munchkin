74a96b2aeb2e74ceadd771ff4fa4124eab72e519
이승윤 <yyuunn17@naver.com>
2021-09-04 22:38:30 +0900

    [UPDATE] @Getter 추가

--- admin/src/main/java/kr/hs/entrydsm/admin/usecase/dto/applicant/ApplicantDetailDto.java
+++ admin/src/main/java/kr/hs/entrydsm/admin/usecase/dto/applicant/ApplicantDetailDto.java
@@ -2,8 +2,10 @@ package kr.hs.entrydsm.admin.usecase.dto.applicant;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
--- admin/src/main/java/kr/hs/entrydsm/admin/usecase/dto/applicant/ApplicantDetailResponse.java
+++ admin/src/main/java/kr/hs/entrydsm/admin/usecase/dto/applicant/ApplicantDetailResponse.java
@@ -1,8 +1,10 @@
 package kr.hs.entrydsm.admin.usecase.dto.applicant;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ApplicantDetailResponse {
