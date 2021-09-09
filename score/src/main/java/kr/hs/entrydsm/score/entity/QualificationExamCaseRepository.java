fc9d899bf228a5418486279b6c2df04e910a1091
wjd030811@gmail.com <wjd030811@gmail.com>
2021-09-09 20:18:28 +0900

    fix: 성적 정보 변경 시 변경 전 정보 남아있는 이슈 해결

--- score/src/main/java/kr/hs/entrydsm/score/entity/QualificationExamCaseRepository.java
+++ score/src/main/java/kr/hs/entrydsm/score/entity/QualificationExamCaseRepository.java
@@ -5,4 +5,5 @@ import java.util.Optional;
 public interface QualificationExamCaseRepository {
     Optional<QualificationExamCase> findByReceiptCode(long receiptCode);
     QualificationExamCase save(QualificationExamCase qualificationExamCase);
+    void delete(QualificationExamCase qualificationExamCase);
 }
