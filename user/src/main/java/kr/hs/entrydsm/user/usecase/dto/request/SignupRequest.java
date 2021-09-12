a9b0f20d903248472276da76c989be83d8f5b666
leeseojune <sung07288346@gmail.com>
2021-09-12 23:35:04 +0900

    [FIX] name validate

--- user/src/main/java/kr/hs/entrydsm/user/usecase/dto/request/SignupRequest.java
+++ user/src/main/java/kr/hs/entrydsm/user/usecase/dto/request/SignupRequest.java
@@ -3,6 +3,7 @@ package kr.hs.entrydsm.user.usecase.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -13,6 +14,7 @@ import javax.validation.constraints.Pattern;
 public class SignupRequest extends AccountRequest {
 
     @Pattern(regexp = "[가-힇]{2,10}")
+	@Length(min = 1, max = 5, message = "INVALID NAME")
     @NotNull
     private String name;
 
