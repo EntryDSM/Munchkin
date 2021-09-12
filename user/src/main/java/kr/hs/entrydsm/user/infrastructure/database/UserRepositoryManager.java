53230ca5ba2a0b9203490d9cabc76aa16c7869e3
이승윤 <yyuunn17@naver.com>
2021-09-13 08:21:37 +0900

    [UPDATE]

--- user/src/main/java/kr/hs/entrydsm/user/infrastructure/database/UserRepositoryManager.java
+++ user/src/main/java/kr/hs/entrydsm/user/infrastructure/database/UserRepositoryManager.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @Repository
 public interface UserRepositoryManager extends CrudRepository<User, Long>, UserRepository {
-    @Query(value = "select * from tbl_user as u join tbl_status as s on u.receipt_code = s.receipt_code where u.receipt_code like ?1 and (?2 is null or u.is_daejeon = ?2) and u.telephone_number like ?3 and u.name like ?4 and ((u.application_type = 'COMMON' and ?5) or (u.application_type = 'MEISTER' and ?6) or (u.application_type = 'SOCIAL' and ?7)) and (?8 is null or s.is_printed_arrived = ?8)", nativeQuery = true)
+    @Query(value = "select * from tbl_user as u join tbl_status as s on u.receipt_code = s.receipt_code where (u.receipt_code like ?1) and (?2 is null or u.is_daejeon = ?2) and (u.telephone_number like ?3) and (u.name like ?4) and ((u.application_type = 'COMMON' and ?5) or (u.application_type = 'MEISTER' and ?6) or (u.application_type = 'SOCIAL' and ?7)) and (?8 is null or s.is_printed_arrived = ?8)", nativeQuery = true)
     Page<User> findAllByUserInfo(String receiptCode, Boolean isDaejeon, String telephoneNumber, String name, boolean isCommon, boolean isMeister, boolean isSocial, Boolean isPrintedArrivedQuery, Pageable pageable);
 
     @Override
