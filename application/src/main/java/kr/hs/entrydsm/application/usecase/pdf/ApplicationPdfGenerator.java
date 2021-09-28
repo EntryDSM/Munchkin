92c00f09dfbd6af0dc4f42c7e6ebc59bd30bb632
Kim Daewoong <daewoong.kim@knowre.com>
2021-09-28 19:30:23 +0900

    [UPDATE] pdf 페이지 추가

--- application/src/main/java/kr/hs/entrydsm/application/usecase/pdf/ApplicationPdfGenerator.java
+++ application/src/main/java/kr/hs/entrydsm/application/usecase/pdf/ApplicationPdfGenerator.java
@@ -1,5 +1,14 @@
 package kr.hs.entrydsm.application.usecase.pdf;
 
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.utils.PdfMerger;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.minmaxwidth.MinMaxWidth;
+import com.itextpdf.layout.property.TextAlignment;
+import com.itextpdf.layout.property.VerticalAlignment;
 import kr.hs.entrydsm.application.config.TemplateFileName;
 import kr.hs.entrydsm.application.entity.Applicant;
 import kr.hs.entrydsm.application.usecase.dto.CalculatedScore;
@@ -7,7 +16,10 @@ import kr.hs.entrydsm.application.usecase.dto.PdfData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -26,13 +38,46 @@ public class ApplicationPdfGenerator {
     private byte[] generateApplicationPdf(Applicant applicant, CalculatedScore calculatedScore) {
         PdfData data = pdfDataConverter.applicationToInfo(applicant, calculatedScore);
 
-        ByteArrayOutputStream result = getTemplateFileNames(applicant).parallelStream()
+        ByteArrayOutputStream[] outputStreams = getTemplateFileNames(applicant).parallelStream()
                 .map(template -> templateProcessor.convertTemplateIntoHtmlString(template, data.toMap()))
                 .map(pdfProcessor::convertHtmlToPdf)
-                .reduce(pdfProcessor::concat)
-                .orElseGet(() -> (ByteArrayOutputStream) ByteArrayOutputStream.nullOutputStream());
+                .toArray(ByteArrayOutputStream[]::new);
 
-        return result.toByteArray();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PdfDocument mergedDocument = new PdfDocument(new PdfWriter(outputStream));
+        PdfMerger pdfMerger = new PdfMerger(mergedDocument);
+        Document document = new Document(mergedDocument);
+
+        for (ByteArrayOutputStream pdfStream : outputStreams) {
+            PdfDocument pdfDoc = getPdfDocument(pdfStream);
+            mergeDocument(pdfMerger, pdfDoc);
+        }
+
+        for (int i = 1; i <= mergedDocument.getNumberOfPages(); i++) {
+            document.showTextAligned(new Paragraph(String.format("- %s -", i)),
+                    300, 25, i, TextAlignment.CENTER, VerticalAlignment.BOTTOM, 0);
+        }
+
+        document.close();
+
+        return outputStream.toByteArray();
+    }
+
+    private PdfDocument getPdfDocument(ByteArrayOutputStream outputStream) {
+        try {
+            InputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+            return new PdfDocument(new PdfReader(inputStream));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void mergeDocument(PdfMerger merger, PdfDocument document) {
+        if (document != null) {
+            merger.merge(document, 1, document.getNumberOfPages());
+            document.close();
+        }
     }
 
     private List<String> getTemplateFileNames(Applicant applicant) {
