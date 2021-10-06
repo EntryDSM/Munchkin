2d141cdaba20a7a223c17a2ac35185fc73c23fed
leeseojune <sung07288346@gmail.com>
2021-10-06 14:41:11 +0900

    [FIX] Null check

--- score/src/main/java/kr/hs/entrydsm/score/usecase/dto/UpdateQualificationExamRequest.java
+++ score/src/main/java/kr/hs/entrydsm/score/usecase/dto/UpdateQualificationExamRequest.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
 @Builder
@@ -18,5 +20,6 @@ public class UpdateQualificationExamRequest {
     @DecimalMin(value = "60.0")
     @DecimalMax(value = "100.0")
     @Digits(integer = 3, fraction = 2)
+	@NotNull(message = "ged_average_score는 null이면 안됩니다.")
     private BigDecimal gedAverageScore;
 }
