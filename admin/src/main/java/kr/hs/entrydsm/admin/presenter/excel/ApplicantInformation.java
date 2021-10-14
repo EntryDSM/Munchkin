676b3b4c3273f948065cd1bb752f07db78a68dfa
이승윤 <yyuunn17@naver.com>
2021-10-14 12:39:16 +0900

    [UPDATE]

--- admin/src/main/java/kr/hs/entrydsm/admin/presenter/excel/ApplicantInformation.java
+++ admin/src/main/java/kr/hs/entrydsm/admin/presenter/excel/ApplicantInformation.java
@@ -50,44 +50,41 @@ public class ApplicantInformation {
         sheet.setColumnWidth(14, 3500);
         sheet.setColumnWidth(15, 4000);
 
-        row.createCell(16).setCellValue("국어 직전전 학기");
-        row.createCell(17).setCellValue("국어 직전 학기");
-        row.createCell(18).setCellValue("국어 3학년 1학기");
-        row.createCell(19).setCellValue("국어 3학년 2학기");
+        row.createCell(16).setCellValue("국어 3학년 2학기");
+        row.createCell(17).setCellValue("사회 3학년 2학기");
+        row.createCell(18).setCellValue("역사 3학년 2학기");
+        row.createCell(19).setCellValue("수학 3학년 2학기");
+        row.createCell(20).setCellValue("과학 3학년 2학기");
+        row.createCell(21).setCellValue("기술가정 3학년 2학기");
+        row.createCell(22).setCellValue("영어 3학년 2학기");
 
-        row.createCell(20).setCellValue("사회 직전전 학기");
-        row.createCell(21).setCellValue("사회 직전 학기");
-        row.createCell(22).setCellValue("사회 3학년 1학기");
-        row.createCell(23).setCellValue("사회 3학년 2학기");
+        row.createCell(23).setCellValue("국어 3학년 1학기");
+        row.createCell(24).setCellValue("사회 3학년 1학기");
+        row.createCell(25).setCellValue("역사 3학년 1학기");
+        row.createCell(26).setCellValue("수학 3학년 1학기");
+        row.createCell(27).setCellValue("과학 3학년 1학기");
+        row.createCell(28).setCellValue("기술가정 3학년 1학기");
+        row.createCell(29).setCellValue("영어 3학년 1학기");
 
-        row.createCell(24).setCellValue("역사 직전전 학기");
-        row.createCell(25).setCellValue("역사 직전 학기");
-        row.createCell(26).setCellValue("역사 3학년 1학기");
-        row.createCell(27).setCellValue("역사 3학년 2학기");
+        row.createCell(30).setCellValue("국어 직전 학기");
+        row.createCell(31).setCellValue("사회 직전 학기");
+        row.createCell(32).setCellValue("역사 직전 학기");
+        row.createCell(33).setCellValue("수학 직전 학기");
+        row.createCell(34).setCellValue("과학 직전 학기");
+        row.createCell(35).setCellValue("기술가정 직전 학기");
+        row.createCell(36).setCellValue("영어 직전 학기");
 
-        row.createCell(28).setCellValue("수학 직전전 학기");
-        row.createCell(29).setCellValue("수학 직전 학기");
-        row.createCell(30).setCellValue("수학 3학년 1학기");
-        row.createCell(31).setCellValue("수학 3학년 2학기");
+        row.createCell(37).setCellValue("국어 직전전 학기");
+        row.createCell(38).setCellValue("사회 직전전 학기");
+        row.createCell(39).setCellValue("역사 직전전 학기");
+        row.createCell(40).setCellValue("수학 직전전 학기");
+        row.createCell(41).setCellValue("과학 직전전 학기");
+        row.createCell(42).setCellValue("기술가정 직전전 학기");
+        row.createCell(43).setCellValue("영어 직전전 학기");
 
-        row.createCell(32).setCellValue("과학 직전전 학기");
-        row.createCell(33).setCellValue("과학 직전 학기");
-        row.createCell(34).setCellValue("과학 3학년 1학기");
-        row.createCell(35).setCellValue("과학 3학년 2학기");
-
-        row.createCell(36).setCellValue("기술가정 직전전 학기");
-        row.createCell(37).setCellValue("기술가정 직전 학기");
-        row.createCell(38).setCellValue("기술가정 3학년 1학기");
-        row.createCell(39).setCellValue("기술가정 3학년 2학기");
-
-        row.createCell(40).setCellValue("영어 직전전 학기");
-        row.createCell(41).setCellValue("영어 직전 학기");
-        row.createCell(42).setCellValue("영어 3학년 1학기");
-        row.createCell(43).setCellValue("영어 3학년 2학기");
-
-        row.createCell(44).setCellValue("직전전 학기 성적 총합");
+        row.createCell(44).setCellValue("3학년 성적 총합");
         row.createCell(45).setCellValue("직전 학기 성적 총합");
-        row.createCell(46).setCellValue("3학년 성적 총합");
+        row.createCell(46).setCellValue("직전전 학기 성적 총합");
         row.createCell(47).setCellValue("교과성적환산점수");
 
         for (int index = 16 ; index <= 47 ; index++) { sheet.setColumnWidth(index, 5000); }
