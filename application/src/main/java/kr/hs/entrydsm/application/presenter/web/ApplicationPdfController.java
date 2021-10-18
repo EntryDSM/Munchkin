6a8d969bc519b1533e4f601f2c3209e577d99733
이승윤 <yyuunn17@naver.com>
2021-10-18 15:51:10 +0900

    Revert "Merge remote-tracking branch 'origin/main' into main"
    This reverts commit f6e1418ee70eaae3df771a96363fb5f474b76916.

--- application/src/main/java/kr/hs/entrydsm/application/presenter/web/ApplicationPdfController.java
+++ application/src/main/java/kr/hs/entrydsm/application/presenter/web/ApplicationPdfController.java
@@ -15,7 +15,6 @@ import java.nio.charset.StandardCharsets;
 
 @Published
 @RequiredArgsConstructor
-@ScheduleInRequired
 @JWTRequired
 @RestController
 @RequestMapping("/pdf")
--- common/src/main/java/kr/hs/entrydsm/common/context/auth/token/JWTTokenProvider.java
+++ common/src/main/java/kr/hs/entrydsm/common/context/auth/token/JWTTokenProvider.java
@@ -9,7 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 import java.util.Date;
 
 @Component
