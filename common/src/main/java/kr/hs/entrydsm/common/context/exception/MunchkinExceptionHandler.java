412f5cd9ad911db13c4529b059b7100f8dc5fa7a
leeseojune <sung07288346@gmail.com>
2021-10-18 17:13:59 +0900

    ⚡ :: stackTrace

--- common/src/main/java/kr/hs/entrydsm/common/context/exception/MunchkinExceptionHandler.java
+++ common/src/main/java/kr/hs/entrydsm/common/context/exception/MunchkinExceptionHandler.java
@@ -6,6 +6,7 @@ import org.apache.tomcat.util.http.fileupload.FileUploadBase.SizeLimitExceededEx
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -19,8 +20,16 @@ public class MunchkinExceptionHandler {
                 HttpStatus.valueOf(errorCode.getStatus()));
     }
 
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	protected ResponseEntity<ErrorResponse> handleNotReadableException(final HttpMessageNotReadableException e) {
+    	final ErrorCode errorCode = ErrorCode.REQUEST_BODY_IS_NULL;
+		return new ResponseEntity<>(new ErrorResponse(errorCode.getStatus(), errorCode.getCode(), errorCode.getMessage()),
+				HttpStatus.valueOf(errorCode.getStatus()));
+	}
+
     @ExceptionHandler(RuntimeException.class)
 	protected ResponseEntity<ErrorResponse> handleRuntimeException(final RuntimeException e) {
+    	e.printStackTrace();
 		return new ResponseEntity<>(new ErrorResponse(500, "Internal Server Error", Arrays.toString(e.getStackTrace())),
 				HttpStatus.valueOf(500));
 	}
