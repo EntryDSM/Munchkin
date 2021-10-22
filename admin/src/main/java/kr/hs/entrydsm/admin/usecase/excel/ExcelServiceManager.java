07dda3a7494c90472ced9271a2d20a72257d0b35
이승윤 <yyuunn17@naver.com>
2021-10-23 01:46:09 +0900

    []이젠 진짜 모르겠어요 지금 13시간 째 하는 중

--- admin/src/main/java/kr/hs/entrydsm/admin/usecase/excel/ExcelServiceManager.java
+++ admin/src/main/java/kr/hs/entrydsm/admin/usecase/excel/ExcelServiceManager.java
@@ -370,6 +370,7 @@ public class ExcelServiceManager implements ExcelService {
         try {
             response = restTemplate.postForObject(url, rq, String.class);
         } catch(Exception e) {
+            System.out.println(routeBody);
             System.out.println(Objects.requireNonNull(rq.getBody()));
         }
         JsonNode document = mapper.readTree(response).path("features").get(0);
